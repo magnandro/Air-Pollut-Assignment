b2754d04e2cfce918905c3629d31a0d16f17c2eb
Alejandro Moreno Fresneda <magnandro@hotmail.com>
2020-09-07 23:23:04 -0500

    Commit Inicial

--- corr.R
+++ corr.R
@@ -16,3 +16,17 @@ corr <- function(directory, threshold =0){
 cr <- corr("specdata")
 summary(cr)
 length(cr)
+
+
+cr <- corr("specdata")                
+cr <- sort(cr)   
+RNGversion("3.5.1")
+set.seed(868)                
+out <- round(cr[sample(length(cr), 5)], 4)
+print(out)
+
+cr <- corr("specdata", 2000)                
+n <- length(cr)                
+cr <- corr("specdata", 1000)                
+cr <- sort(cr)
+print(c(n, round(cr, 4)))
